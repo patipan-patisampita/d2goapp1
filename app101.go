a7bf7536a8c1696407398197cc2bdc1fc0865cf7
patipan-patisampita <patipan.patisampita@gmail.com>
2021-11-05 16:27:48 +0700

    initail commit

---
+++ app101.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int
+	var s string
+	i = 10
+	s = "Thailand"
+	fmt.Println(i)
+	fmt.Println(s)
+}
---
+++ hello.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int = 10 //local varible
+	var s string = "Thailand" //local variable
+	fmt.Println("Hello World!")
+	fmt.Println(i)
+	fmt.Print(s)
+}
